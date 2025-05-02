b905a345e4e24c761eff7de7a08fe2ff69788170
gwelle <guillaume.welle.sio@gmail.com>
2025-05-02 12:14:54 +0200

    first unit tests

--- tests/Unit/Entity/DinosaurtTest.php
+++ tests/Unit/Entity/DinosaurtTest.php
@@ -6,8 +6,21 @@ use PHPUnit\Framework\TestCase;
 
 class DinosaurtTest extends TestCase
 {
+    /**
+     * @test("test with assertEquals")
+     */
     public function testItWorks(): void
     {
-        self::assertEquals(42, 42);
+        //self::assertEquals(42, 42);
+        self::assertEquals('42', 42); // 42 == '42'
+    }
+
+    /**
+     * @test("test with assertSame")
+     */
+    public function testTtWorksTheSame():void
+    {
+        //self::assertSame(42, 42);
+        self::assertSame('42', 42); // 42 === '42'
     }
 }
